af1877519661c9de08b78c86735e0bcca540cda0
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-04-28 21:02:36 -0400

    modification de Documents.js

---
+++ linatp4/src/components/document/Documents.js
@@ -0,0 +1,15 @@
+import Document from "./Document";
+const Documents = ({documents, onDelete, onToggle}) => {
+    return (
+        <>
+            {documents.map((document) => (
+                <Document key={document.id}
+                          document={document}
+                          onDelete={onDelete}
+                          onToggle={onToggle}/>
+            ))}
+        </>
+    )
+}
+
+export default Documents
