4fe8e7031db13529941070e9aa19108aac766cec
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-04-30 18:36:20 -0400

    modification dans index

--- linatp4/src/App.js
+++ linatp4/src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import './App.css';
 import Header from "./components/Header";
@@ -9,104 +9,46 @@ import Documents from "./components/document/Documents";
 import AddDocument from "./components/document/AddDocument";
 
 function App() {
-
-    const [showAddClient, setShowAddClient] = useState(false)
     const [clients, setClients] = useState([])
-    const fetchClients = async () => {
-        const res = await fetch('http://localhost:8080/clients')
-        const data = await res.json()
-        return data
-    }
-    const addClient = async (client) => {
-        const res = await fetch('http://localhost:8080/clients',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json',
-                },
-                body: JSON.stringify(client)
-            })
-            const data = await res.json()
-            setClients([...clients, data])
-    }
-    const deleteClient = async (id) => {
-        await fetch(`http://localhost:8080/clients/${id}`,
-            {
-                method: 'DELETE'
-            })
-            setClients(clients.filter((client) => client.id !== id))
-    }
-
-    const [showAddDocument, setShowAddDocument] = useState(false)
     const [documents, setDocuments] = useState([])
-    const fetchDocuments = async () => {
-        const res = await fetch('http://localhost:8080/documents')
-        const data = await res.json()
-        return data
-    }
-    const addDocument = async (document) => {
-        const res = await fetch('http://localhost:8080/documents',
-            {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json',
-                },
-                body: JSON.stringify(document)
-            })
-        const data = await res.json()
-        setDocuments([...documents, data])
-    }
-    const deleteDocument = async (id) => {
-        await fetch(`http://localhost:8080/documents/${id}`,
-            {
-                method: 'DELETE'
-            })
-            setDocuments(documents.filter((document) => document.id !== id))
-    }
-
-    useEffect(() => {
-        const getClients = async () => {
-            const clientsFromServer = await fetchClients()
-            setClients(clientsFromServer)
-        }
-        const getDocuments = async () => {
-            const documentsFromServer = await fetchDocuments()
-            setDocuments(documentsFromServer)
-        }
-        getClients()
-        getDocuments()
-    }, [])
 
     return (
-        <div className='container'>
-            <Header title='Client'
-                    onAdd={() =>
-                        setShowAddClient(!showAddClient)}
-                    showAdd={showAddClient}/>
-            <Route path='/' exact render={(props) => (
-                <>
-                    {showAddClient && <AddClient onAdd={addClient} />}
-                    {clients.length > 0 ?
-                        <Clients clients={clients}
-                                 onDelete={deleteClient}/>
-                        : 'No Clients'}
-                </>
-            )} />
-            <Header title='Document'
-                    onAdd={() =>
-                        setShowAddDocument(!showAddDocument)}
-                    showAdd={showAddDocument}/>
-            <Route path='/' exact render={(props) => (
-                <>
-                    {showAddDocument && <AddDocument onAdd={addDocument} />}
-                    {documents.length > 0 ?
-                        <Documents documents={documents}
-                                   onDelete={deleteDocument} />
-                        : 'No Documents'}
-                </>
-            )} />
-        </div>
+        <Router>
+            <div className='container'>
+                <Routes>
+                    <Route path="/" element={<Header title={'Bibliotheque'}/>}/>
+                    <Route path="/clients" element={<Clients/>}/>
+                    <Route path="/documents" element={<Documents/>}/>
+                </Routes>
+            </div>
+        </Router>
     );
+                    /*<Header title='Client'
+                            onAdd={() =>
+                                setShowAddClient(!showAddClient)}
+                            showAdd={showAddClient}/>
+                    <Route path='/' exact render={(props) => (
+                        <>
+                            {showAddClient && <AddClient onAdd={addClient} />}
+                            {clients.length > 0 ?
+                                <Clients clients={clients}
+                                         onDelete={deleteClient}/>
+                                : 'No Clients'}
+                        </>
+                    )} />
+                    <Header title='Document'
+                            onAdd={() =>
+                                setShowAddDocument(!showAddDocument)}
+                            showAdd={showAddDocument}/>
+                    <Route path='/' exact render={(props) => (
+                        <>
+                            {showAddDocument && <AddDocument onAdd={addDocument} />}
+                            {documents.length > 0 ?
+                                <Documents documents={documents}
+                                           onDelete={deleteDocument} />
+                                : 'No Documents'}
+                        </>
+                    )} />*/
 }
 
 export default App;
--- linatp4/src/components/Header.js
+++ linatp4/src/components/Header.js
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
+import {Link} from "react-router-dom";
 
 
-const Header = ({title, onAdd, showAdd}) => {
+const Header = ({title}) => {
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'}
-                    text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
+            <Link to='/clients'><Button color={'blue'}
+                          text={'Clients'}/>
+            </Link>
+            <Link to='/documents'><Button color={'green'}
+                                        text={'Documents'}/>
+            </Link>
         </header>
     )
 }
