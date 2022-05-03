380b66253db905997be2502a1788a63a381c1aa1
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-05-03 14:12:10 -0400

    ajout pour page non trouvee

--- linatp4/src/App.js
+++ linatp4/src/App.js
@@ -1,5 +1,4 @@
-import {useState, useEffect} from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {useState} from 'react'
 
 import './App.css';
 import Header from "./components/Header";
@@ -9,46 +8,112 @@ import Documents from "./components/document/Documents";
 import AddDocument from "./components/document/AddDocument";
 
 function App() {
-    const [clients, setClients] = useState([])
-    const [documents, setDocuments] = useState([])
+
+    const [showAddClient, setShowAddClient] = useState(false)
+    const [clients, setClients] = useState(
+        [
+            {
+                id: 1,
+                nom: "John",
+                prenom: "Smith",
+                rue: "Daragon",
+                ville: "Montreal",
+                codePostal: "H05C42",
+                numeroTelephone: "514-900-5698",
+                dateInscription: "2022/02/20",
+            },
+            {
+                id: 2,
+                nom: "Marvin",
+                prenom: "Stewart",
+                rue: "LaSale",
+                ville: "Montreal",
+                codePostal: "H05C53",
+                numeroTelephone: "514-900-7643",
+                dateInscription: "2022/02/22",
+            }
+        ]
+    )
+
+    const addClient = (client) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newClient = {id, ...client}
+        setClients([...clients, newClient])
+    }
+
+    const deleteClient = (id) => {
+        setClients(clients.filter((client) => client.id !== id))
+    }
+
+    const [showAddDocument, setShowAddDocument] = useState(false)
+    const [documents, setDocuments] = useState(
+        [
+            {
+                id: 1,
+                etatDocument: "DISPONIBLE",
+                genreDocument: "CD",
+                titre: "harry potter",
+                auteur: "JK. Rolling",
+                editeur: "maison edition",
+                anneePublication: "2000",
+                nbrExemplaire: "3",
+            },
+            {
+                id: 2,
+                etatDocument: "ENDOMMAGE",
+                genreDocument: "DVD",
+                titre: "bobby bob",
+                auteur: "lilo lee",
+                editeur: "edition bop",
+                anneePublication: "2018",
+                nbrExemplaire: "5",
+            },
+            {
+                id: 3,
+                etatDocument: "EMPRUNTE",
+                genreDocument: "livre",
+                titre: "avengers",
+                auteur: "Josh whedon",
+                editeur: "marvel",
+                anneePublication: "2020",
+                nbrExemplaire: "2",
+            }
+        ]
+    )
+
+    const addDocument = (document) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newDocument = {id, ...document}
+        setDocuments([...documents, newDocument])
+    }
+
+    const deleteDocument = (id) => {
+        setDocuments(documents.filter((document) => document.id !== id))
+    }
 
     return (
-        <Router>
-            <div className='container'>
-                <Routes>
-                    <Route path="/" element={<Header title={'Bibliotheque'}/>}/>
-                    <Route path="/clients" element={<Clients/>}/>
-                    <Route path="/documents" element={<Documents/>}/>
-                </Routes>
-            </div>
-        </Router>
+        <div className='container'>
+            <h1>Bibliotheque de JavaTown</h1>
+            <Header title='Client'
+                    onAdd={() =>
+                        setShowAddClient(!showAddClient)}
+                    showAdd={showAddClient}/>
+            {showAddClient && <AddClient onAdd={addClient} />}
+            {clients.length > 0 ?
+                <Clients clients={clients}
+                         onDelete={deleteClient}/>
+                : 'No Clients'}
+            <Header title='Document'
+                    onAdd={() =>
+                        setShowAddDocument(!showAddDocument)}
+                    showAdd={showAddDocument}/>
+            {showAddDocument && <AddDocument onAdd={addDocument} />}
+            {documents.length > 0 ?
+                <Documents documents={documents}
+                           onDelete={deleteDocument} />
+                : 'No Documents'}
+        </div>
     );
-                    /*<Header title='Client'
-                            onAdd={() =>
-                                setShowAddClient(!showAddClient)}
-                            showAdd={showAddClient}/>
-                    <Route path='/' exact render={(props) => (
-                        <>
-                            {showAddClient && <AddClient onAdd={addClient} />}
-                            {clients.length > 0 ?
-                                <Clients clients={clients}
-                                         onDelete={deleteClient}/>
-                                : 'No Clients'}
-                        </>
-                    )} />
-                    <Header title='Document'
-                            onAdd={() =>
-                                setShowAddDocument(!showAddDocument)}
-                            showAdd={showAddDocument}/>
-                    <Route path='/' exact render={(props) => (
-                        <>
-                            {showAddDocument && <AddDocument onAdd={addDocument} />}
-                            {documents.length > 0 ?
-                                <Documents documents={documents}
-                                           onDelete={deleteDocument} />
-                                : 'No Documents'}
-                        </>
-                    )} />*/
 }
 
 export default App;
