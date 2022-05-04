bbf0417d0b8a31b0ac367f6515b33704512d7182
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-05-04 17:31:06 -0400

    ajout de fetch pour la page document (add, delete, useEffect)

--- linatp4/src/components/document/PageDocument.js
+++ linatp4/src/components/document/PageDocument.js
@@ -1,51 +1,51 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Header2 from "../Header2";
 import Documents from "./Documents";
 import AddDocument from "./AddDocument";
 
 const PageDocument = () => {
 
-    const [showAddDocument, setShowAddDocument] = useState(false)
+    useEffect(() => {
+        const getDocuments = async () => {
+            const documentsFromServer = await fetchDocuments()
+            setDocuments(documentsFromServer)
+        }
+        getDocuments()
+    }, [])
 
-    const [documents, setDocuments] = useState([{
-        id: 1,
-        etatDocument: "DISPONIBLE",
-        genreDocument: "CD",
-        titre: "harry potter",
-        auteur: "JK. Rolling",
-        editeur: "maison edition",
-        anneePublication: "2000",
-        nbrExemplaire: "3",
-    },
-        {
-            id: 2,
-            etatDocument: "ENDOMMAGE",
-            genreDocument: "DVD",
-            titre: "bobby bob",
-            auteur: "lilo lee",
-            editeur: "edition bop",
-            anneePublication: "2018",
-            nbrExemplaire: "5",
-        },
-        {
-            id: 3,
-            etatDocument: "EMPRUNTE",
-            genreDocument: "livre",
-            titre: "avengers",
-            auteur: "Josh whedon",
-            editeur: "marvel",
-            anneePublication: "2020",
-            nbrExemplaire: "2",
-        }])
-
-    const addDocument = (document) => {
-        const id = Math.floor(Math.random() * 10000) + 1
-        const newDocument = {id, ...document}
-        setDocuments([...documents, newDocument])
+    const fetchDocuments = async () => {
+        const res = await fetch('http://localhost:8080/documents')
+        const data = await res.json()
+        return data
     }
 
-    const deleteDocument = (id) => {
+    const [showAddDocument, setShowAddDocument] = useState(false)
+    const [documents, setDocuments] = useState([])
+
+    const addDocument = async (document) => {
+        const res = await fetch('http://localhost:8080/documents',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                body: JSON.stringify(document)
+            })
+        const data = await res.json()
+        setDocuments([...documents, data])
+
+        /*const id = Math.floor(Math.random() * 10000) + 1
+        const newDocument = {id, ...document}
+        setDocuments([...documents, newDocument])*/
+    }
+
+    const deleteDocument = async (id) => {
+        await fetch(`http://localhost:8080/documents/${id}`, {
+            method: 'DELETE'
+        })
         setDocuments(documents.filter((document) => document.id !== id))
+
+        //setDocuments(documents.filter((document) => document.id !== id))
     }
 
     return (
