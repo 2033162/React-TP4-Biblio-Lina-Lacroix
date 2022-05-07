677fe3f7b2f4b4bdb4ff6fc6764968f67853e7ea
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-05-07 07:02:14 -0400

    ajout des updates pour cd, dvd, livre

--- src/main/java/com/lina/programme_biblio_tp4/controllers/DocumentControllerReact.java
+++ src/main/java/com/lina/programme_biblio_tp4/controllers/DocumentControllerReact.java
@@ -62,19 +62,59 @@ public class DocumentControllerReact {
         return serviceDocument.saveDocument(dvdForm.toDocument());
     }
 
-    @PatchMapping("/{id}")
-    public DocumentForm updateDocument(@PathVariable long id, @RequestBody DocumentForm documentFormDetail) {
-        DocumentForm document = serviceDocument.getDocument(id).toDocumentForm();
+    @PatchMapping("/livres/{id}")
+    public DocumentForm updateLivre(@PathVariable long id,
+                                       @RequestBody LivreForm livreFormDetail) {
+        LivreForm livre = (LivreForm) serviceDocument.getLivre(id).orElseThrow(RuntimeException::new).toDocumentForm();
 
-        document.setEtatDocument(documentFormDetail.getEtatDocument());
-        document.setGenreDocument(documentFormDetail.getGenreDocument());
-        document.setTitre(documentFormDetail.getTitre());
-        document.setAuteur(documentFormDetail.getAuteur());
-        document.setEditeur(documentFormDetail.getEditeur());
-        document.setAnneePublication(documentFormDetail.getAnneePublication());
-        document.setNbrExemplaire(documentFormDetail.getNbrExemplaire());
+        livre.setGenreDocument(livreFormDetail.getGenreDocument());
+        livre.setEtatDocument(livreFormDetail.getEtatDocument());
+        livre.setTitre(livreFormDetail.getTitre());
+        livre.setAuteur(livreFormDetail.getAuteur());
+        livre.setEditeur(livreFormDetail.getEditeur());
+        livre.setAnneePublication(livreFormDetail.getAnneePublication());
+        livre.setNbrExemplaire(livreFormDetail.getNbrExemplaire());
+        livre.setNbrPages(livreFormDetail.getNbrPages());
+        livre.setGenreLivre(livreFormDetail.getGenreLivre());
 
-        return serviceDocument.saveDocument(document.toDocument());
+        return serviceDocument.saveDocument(livre.toDocument());
+    }
+
+    @PatchMapping("/cds/{id}")
+    public DocumentForm updateCd(@PathVariable long id,
+                                       @RequestBody CdForm cdFormDetail) {
+        CdForm cd = (CdForm) serviceDocument.getCD(id).orElseThrow(RuntimeException::new).toDocumentForm();
+
+        cd.setEtatDocument(cdFormDetail.getEtatDocument());
+        cd.setGenreDocument(cdFormDetail.getGenreDocument());
+        cd.setTitre(cdFormDetail.getTitre());
+        cd.setEditeur(cdFormDetail.getEditeur());
+        cd.setAuteur(cdFormDetail.getAuteur());
+        cd.setAnneePublication(cdFormDetail.getAnneePublication());
+        cd.setNbrExemplaire(cdFormDetail.getNbrExemplaire());
+        cd.setGenreMusique(cdFormDetail.getGenreMusique());
+        cd.setCompositeur(cdFormDetail.getCompositeur());
+        cd.setInterprete(cdFormDetail.getInterprete());
+
+        return serviceDocument.saveDocument(cd.toDocument());
+    }
+
+    @PatchMapping("/dvds/{id}")
+    public DocumentForm updateDvd(@PathVariable long id,
+                                 @RequestBody DvdForm dvdFormDetail) {
+        DvdForm dvd = (DvdForm) serviceDocument.getDVD(id).orElseThrow(RuntimeException::new).toDocumentForm();
+
+        dvd.setEtatDocument(dvdFormDetail.getEtatDocument());
+        dvd.setGenreDocument(dvdFormDetail.getGenreDocument());
+        dvd.setTitre(dvdFormDetail.getTitre());
+        dvd.setAuteur(dvdFormDetail.getAuteur());
+        dvd.setEditeur(dvdFormDetail.getEditeur());
+        dvd.setAnneePublication(dvdFormDetail.getAnneePublication());
+        dvd.setNbrExemplaire(dvdFormDetail.getNbrExemplaire());
+        dvd.setDuree(dvdFormDetail.getDuree());
+        dvd.setGenreFilm(dvdFormDetail.getGenreFilm());
+
+        return serviceDocument.saveDocument(dvd.toDocument());
     }
 
     @DeleteMapping("/{id}")
