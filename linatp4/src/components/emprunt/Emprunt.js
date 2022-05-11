0163c867c2ec7f06c6864ed2f79cf2d543395279
20331622@claurendeau.qc.ca <2033162@claurendeau.qc.ca>
2022-05-11 00:30:07 -0400

    modifications pour ajouter parametre genreDocument dans l'affichage react

--- linatp4/src/components/emprunt/Emprunt.js
+++ linatp4/src/components/emprunt/Emprunt.js
@@ -2,7 +2,7 @@ const Emprunt = ({emprunt}) => {
     return (
         <div className="emprunt">
             <h3>{emprunt.clientDto.prenom} {emprunt.clientDto.nom}</h3>
-            <p>{emprunt.documentDto.titre}</p>
+            <p>{emprunt.documentDto.genreDocument} {emprunt.documentDto.titre}</p>
         </div>
     )
 }
